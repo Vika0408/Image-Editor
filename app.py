db3df268002135214e709694c8914613abeff27a
Vika0408 <palvanshika609@gmail.com>
2024-04-14 18:27:14 +0530

    Image Editor

--- app.py
+++ app.py
@@ -13,5 +13,12 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/edit', methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        return "POST request is here"
+    return render_template("index.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
