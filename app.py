44be2adab92f4571e5653ed23fef9339e3fc86f5
Vika0408 <palvanshika609@gmail.com>
2024-04-20 16:56:58 +0530

    Image Editor

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask, render_template
+import os
+import cv2
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def processimage(filename, operation) :
+    print(f"The operation is {operation} and the filename is {filename}")
+    img = cv2.imread(f"uploads/{filename}")
+    match operation:
+        case "cgray" :
+            imgProcess = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/{filename}", imgProcess)
+
+    pass
 
 
 @app.route('/')
@@ -16,7 +39,23 @@ def about():
 @app.route('/edit', methods=["GET", "POST"])
 def edit():
     if request.method == "POST":
-        return "POST request is here"
+        operation = request.form.get("operation")
+        if 'file' not in request.files:
+            flash('No file part')
+            return "error"
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return "error no selected file"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            processimage(filename, operation)
+            flash(f"Your image has been processed and is available <a href='/static/{filename}'>here</a>")
+            return render_template("index.html")
+
     return render_template("index.html")
 
 
