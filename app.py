c0b3ac4f8ae2ef277ca1e0c21e0c9ec39997eece
Vika <palvanshika609@gmail.com>
2024-05-07 10:45:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def edit():
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+'''if __name__ == '__main__':
+    app.run(debug=True, port=5001)'''
